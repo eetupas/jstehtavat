1cf699681558304db3954706d87705d1052c1bcd
eetupas <79096066+eetupas@users.noreply.github.com>
2023-01-29 23:04:00 +0200

    init

--- src/index.js
+++ src/index.js
@@ -40,3 +40,13 @@ langFi.addEventListener('click', () => {
 random.addEventListener('click', () => {
     alert(sdata.getRandomDish(sdata.activeMenu));
 });
+
+if ('serviceWorker' in navigator) {
+ window.addEventListener('load', () => {
+   navigator.serviceWorker.register('/service-worker.js').then(registration => {
+     console.log('SW registered: ', registration);
+   }).catch(registrationError => {
+     console.log('SW registration failed: ', registrationError);
+   });
+ });
+}
